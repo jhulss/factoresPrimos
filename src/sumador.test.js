04fafcdf27568a0d8c687f0c9632ab4df19b7248
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-09-24 17:23:16 -0400

    Tercera prueba, se ingresa un numero primo, en este caso 4 y se añaden a la lista sus factores primos [2,2]

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -12,5 +12,11 @@ describe("Factores primos", () => {
     expect(calcular.calcularFactores(2)).toEqual([2]);
   });
 
+  it("deberia poder calcular el factor primo de 4 y devolver sus factores en la lista", () => {
+    const calcular = new factoresPrimos();
+    expect(calcular.calcularFactores(4)).toEqual([2,2]);
+  });
+
+  
 
 });
