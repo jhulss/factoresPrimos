b6cf0bf4836dae7815affb97ad09d7963f260126
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-09-24 17:16:48 -0400

    Segunda prueba, se añade un numero primo a la lista, en este caso 2

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -7,4 +7,10 @@ describe("Factores primos", () => {
     expect(calcular.calcularFactores(1)).toEqual([]);
   });
 
+  it("deberia ingresar un numero mayor a 1, en este caso 2 ya que dos es un numero primo", () => {
+    const calcular = new factoresPrimos();
+    expect(calcular.calcularFactores(2)).toEqual([2]);
+  });
+
+
 });
